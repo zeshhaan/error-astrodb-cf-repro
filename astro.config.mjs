562e2009b874436f7b15fcdf5ee1a5174f593ca4
Mohammed Zeeshan <work.zeeshan@gmail.com>
2024-09-18 14:09:44 +0530

    astro db not working on cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [db()]
 });
